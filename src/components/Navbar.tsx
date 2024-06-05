dea8b1d7f1a27b6af58560b14a143a93539f54fc
Rakshit Sisodiya <sisodiarakshit456@gmail.com>
2024-06-05 11:21:17 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = async () => {
   return (
     <>
     
-    <div className='fixed top-0 inset-x-0 h-fit bg-slate-700 border-b border-zinc-500 z-[10] py-2'>
+    <div className='fixed top-0 inset-x-0 h-fit bg-slate-600 border-b border-zinc-500 z-[10] py-2'>
         <div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
             <Link href='/' className='flex gap-2 items-center'>
                 <Icons.logo className='h-8 w-8 sm:h-6 sm:w-6' />
