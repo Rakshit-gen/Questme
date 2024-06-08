07489333f35b2848b9e30277f651fdc8d336119f
Rakshit Sisodiya <sisodiarakshit456@gmail.com>
2024-06-08 14:01:08 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default async function Home() {
           <dl className='-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6'>
             <div className='flex justify-between gap-x-4 py-3'>
               <p className='text-zinc-500'>
-                Your personal Breadit frontpage. Create your
+                Your personal qME frontpage. Create your
                 own communities and share them.
               </p>
             </div>
